afc98e1ce6facc143da210f94e5f5bb4a3ee8e74
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-10-23 17:30:25 +0200

    feat: add envs and setup for firebase-admin

--- netlify/functions/login.ts
+++ netlify/functions/login.ts
@@ -1,4 +1,24 @@
 import { Config, Context } from "@netlify/functions";
+import firebaseAdmin from "firebase-admin";
+
+const {
+  FIREBASE_ADMIN_CLIENT_EMAIL,
+  FIREBASE_ADMIN_PRIVATE_KEY,
+  FIREBASE_ADMIN_PROJECT_ID,
+} = process.env;
+
+const firebaseAdminCredential = {
+  credential: firebaseAdmin.credential.cert({
+    clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL,
+    privateKey: FIREBASE_ADMIN_PRIVATE_KEY,
+    projectId: FIREBASE_ADMIN_PROJECT_ID,
+  }),
+  //   databaseURL: "https://joe-blog-fake.firebaseio.com/",
+};
+
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp(firebaseAdminCredential);
+}
 
 interface RequestBody {
   token?: string;
