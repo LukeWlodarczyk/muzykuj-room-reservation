c50cc586a1cb636dda474e7615e6060999cd4343
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-10-23 13:59:05 +0200

    feat: update config to netlify func and create helper for reponse

--- netlify/functions/login.ts
+++ netlify/functions/login.ts
@@ -1,9 +1,38 @@
 import { Config, Context } from "@netlify/functions";
 
+interface RequestBody {
+  token?: string;
+}
+
+const createResponse = ({ status, ...body }) =>
+  new Response(JSON.stringify(body), {
+    status,
+    headers: { "Content-Type": "application/json" },
+  });
+
 export default async (req: Request, context: Context) => {
-  return new Response(`Login!`);
+  const body: RequestBody = await req.json();
+
+  const { token: googleToken } = body;
+
+  if (!googleToken) {
+    return createResponse({
+      message: "Unauthorized",
+      token: null,
+      status: 401,
+    });
+  }
+
+  const firebaseCustomToken = "1234567";
+
+  return createResponse({
+    message: "Success",
+    token: firebaseCustomToken,
+    status: 200,
+  });
 };
 
 export const config: Config = {
-  path: "/login",
+  method: "POST",
+  path: "/api/login",
 };
