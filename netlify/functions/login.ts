946d817271186a095040e9ff20bb3d16af12fb12
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-10-23 12:39:09 +0200

    feat: add basic netlify function

---
+++ netlify/functions/login.ts
@@ -0,0 +1,9 @@
+import { Config, Context } from "@netlify/functions";
+
+export default async (req: Request, context: Context) => {
+  return new Response(`Login!`);
+};
+
+export const config: Config = {
+  path: "/login",
+};
