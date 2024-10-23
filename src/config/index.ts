a86bf0707e0a688e1d61cff6e20b9c1f1d1f35a0
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-10-23 16:32:08 +0200

    feat: add firebase

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,13 @@ export default {
   google: {
     oAuthClientId: import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID,
   },
-  firebase: {},
+  firebase: {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  },
 };
