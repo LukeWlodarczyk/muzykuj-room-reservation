91054a66dd3c1adfcf6ffc4a46c886a81b3a0aed
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-11-02 20:44:24 +0100

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import Login from "@/pages/Login";
 
 import config from "@/config";
+
+import Router from "@/router";
+
 import { AuthProvider } from "@/context/auth";
 
 function App() {
   return (
     <GoogleOAuthProvider clientId={config.google.oAuthClientId}>
       <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-          </Routes>
-        </Router>
+        <Router />
       </AuthProvider>
     </GoogleOAuthProvider>
   );
