9492642328bfa6a78729a646ad06433b4fe65b17
Luke Wlodarczyk <luke.wlodarczyk.dev@gmail.com>
2024-11-09 02:25:08 +0100

    feat: add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-brand-purple": "#cad8fa",
+        "light-brand-blue": "#b3f2fc",
+        "brand-blue": "#bce5fb",
+        "dark-brand-blue": "#81d1f8",
+        "brand-black": "#27272a",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
